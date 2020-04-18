2b55148c5e45b7b85af688abd85b1dbc0f0d4789
hunterhug <gdccmcm14@live.com>
2020-04-18 11:43:52 +0800

    gogo

--- code/set/other.go
+++ code/set/other.go
@@ -6,19 +6,21 @@ import (
 )
 
 func main() {
-	a := make(map[int]int, 10000)
+	v := struct{}{}
+
+	a := make(map[int]struct{}, 10000)
 	for i := 0; i < 10000; i++ {
-		a[i] = i
+		a[i] = v
 	}
 
 	runtime.GC()
 	printMemStats("添加1万个键值对后")
 
-	fmt.Println("删除前数组长度：", len(a))
+	fmt.Println("删除前Map长度：", len(a))
 	for i := 0; i < 10000-1; i++ {
 		delete(a, i)
 	}
-	fmt.Println("删除后数组长度：", len(a))
+	fmt.Println("删除后Map长度：", len(a))
 
 	// 再次进行手动GC回收
 	runtime.GC()
