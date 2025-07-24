3db3e3311a5e7091f7012e707db23f354a46d634
hunterhug <hunterhug>
2025-07-24 16:27:46 +0800

    fix: docker

--- docker_debug_docsify.sh
+++ docker_debug_docsify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -p 12346:3000 --name=docsify -v $(pwd):/docs hunterhug/docsify
+docker run -it --rm -p 12346:3000 --name=docsify -v $(pwd):/docs hunterhug/algorithm:docsify
--- docker_run_docsify.sh
+++ docker_run_docsify.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker rm -f algorithm
+docker pull hunterhug/algorithm:docsify
 docker run --name algorithm -d -p 12346:3000 hunterhug/algorithm:docsify
